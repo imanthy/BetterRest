42c94b7ca33dbeb0013723ca2b8180f05939a0b6
Anthy Chen <koyun.chen@gmail.com>
2023-05-03 12:10:31 -0700

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anthy Chen on 5/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
